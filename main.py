27e6d205ecb7695c4fa284e795e86769bb08edd2
Joy <joymakasi@hotmail.co.uk>
2022-02-20 22:31:27 +0000

    updating api

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-from flask import Flask, jsonify, request 
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request
+from http import HTTPStatus  
 from comic_books import books, message
-from flask_cors import CORS
-
 app = Flask(__name__)
-api = Api(app)
-CORS(app)
 
-class Books(Resource):
-    def get(self):
-        return jsonify(books)
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify({'data': books})
 
-    def post(self):
-        data = request.get_json()
-        title = data.get('book_title')
-        author = data.get('book_author')
-        publisher = data.get('publisher')
-        description = data.get('description')
-        book = {
-            "id": len(books)+1,  
-            "title": title,
-            "author": author,
-            "publisher": publisher,
-            "description": description
-            }
-        books.append(book)
-        return jsonify(books), HTTPStatus.CREATED
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'data': books})
 
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = next(
+        (book for book in books if book['id'] ==book_id),None) 
+    if book:
+       return jsonify(book)
+    return jsonify(
+       message),HTTPStatus.NOT_FOUND
 
-class Book(Resource):
-    def get(self, book_id):
-        book = next(
-            (book for book in books if book['id'] ==book_id),None) 
-        if book:
-            return jsonify(book)
-        return jsonify(message),HTTPStatus.NOT_FOUND
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    book = next((book for book in books if book['id'] == book_id),        None) 
+    if not book:
+       return jsonify(
+       message),HTTPStatus.NOT_FOUND
+    data = request.get_json()
+    book.update(
+       {
+        'title': data.get('book_title'),
+        'author': data.get('book_author'),
+        'publisher': data.get('publisher'),
+        'description': data.get('description'),
+        }
+    )
+    return jsonify(book)
 
-    def put(self,book_id):
-        book = next((book for book in books if book['id'] == book_id),None) 
-        if not book:
-            return jsonify(message),HTTPStatus.NOT_FOUND
-        data = request.get_json()
-        book.update(
-        {
-            'title': data.get('book_title'),
-            'author': data.get('book_author'),
-            'publisher': data.get('publisher'),
-            'description': data.get('description'),
-            }
-        )
-        return jsonify(book)
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.get_json()
+    title = data.get('book_title')
+    author = data.get('book_author')
+    publisher = data.get('publisher')
+    description = data.get('description')
+    book = {
+         "id": len(books)+1,  
+         "title": title,
+         "author": author,
+         "publisher": publisher,
+         "description": description
+         }
+    books.append(book)
+    return jsonify(books), HTTPStatus.CREATED
 
-api.add_resource(Books, '/')
-api.add_resource(Book, '//<int:book_id>')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': 
+     app.run()
