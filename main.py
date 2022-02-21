f716fdbd87ac0f973e58ca1c38a60bdb188416b3
Joy <joymakasi@hotmail.co.uk>
2022-02-21 20:44:28 +0000

    changing the code

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ class Books(Resource):
     def get(self):
         return jsonify(books)
 
-    def post(self):
-        data = request.get_json()
-        title = data.get('book_title')
-        author = data.get('book_author')
-        publisher = data.get('publisher')
-        description = data.get('description')
-        book = {
-            "id": len(books)+1,  
-            "title": title,
-            "author": author,
-            "publisher": publisher,
-            "description": description
-            }
-        books.append(book)
-        return jsonify(books), HTTPStatus.CREATED
+    # def post(self):
+    #     data = request.get_json()
+    #     title = data.get('book_title')
+    #     author = data.get('book_author')
+    #     publisher = data.get('publisher')
+    #     description = data.get('description')
+    #     book = {
+    #         "id": len(books)+1,  
+    #         "title": title,
+    #         "author": author,
+    #         "publisher": publisher,
+    #         "description": description
+    #         }
+    #     books.append(book)
+    #     return jsonify(books), HTTPStatus.CREATED
 
+
+class Book(Resource):
     def get(self, book_id):
         book = next(
             (book for book in books if book['id'] ==book_id),None) 
@@ -49,34 +51,25 @@ class Books(Resource):
             }
         )
         return jsonify(book)
-        
 
-
-# class Book(Resource):
-#     def get(self, book_id):
-#         book = next(
-#             (book for book in books if book['id'] ==book_id),None) 
-#         if book:
-#             return jsonify(book)
-#         return jsonify(message),HTTPStatus.NOT_FOUND
-
-    # def put(self,book_id):
-    #     book = next((book for book in books if book['id'] == book_id),None) 
-    #     if not book:
-    #         return jsonify(message),HTTPStatus.NOT_FOUND
-    #     data = request.get_json()
-    #     book.update(
-    #     {
-    #         'title': data.get('book_title'),
-    #         'author': data.get('book_author'),
-    #         'publisher': data.get('publisher'),
-    #         'description': data.get('description'),
-    #         }
-    #     )
-    #     return jsonify(book)
+    def post(self):
+        data = request.get_json()
+        title = data.get('book_title')
+        author = data.get('book_author')
+        publisher = data.get('publisher')
+        description = data.get('description')
+        book = {
+            "id": len(books)+1,  
+            "title": title,
+            "author": author,
+            "publisher": publisher,
+            "description": description
+            }
+        books.append(book)
+        return jsonify(books), HTTPStatus.CREATED
 
 api.add_resource(Books, '/')
-api.add_resource(Books, '//<int:book_id>')
+api.add_resource(Book, '//<int:book_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
