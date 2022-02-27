37d9b6850f6d5f8c3343a47ff0f7a23bfb31baaf
Joy <joymakasi@hotmail.co.uk>
2022-02-27 20:58:24 +0000

    adding new code

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ class Books(Resource):
     def get(self):
         return make_response(jsonify(books))
 
-
-class New(Resource):
     def post(self):
         data = request.post("https://flask-joy-api.herokuapp.com/")
         title = data.get('book_title')
@@ -30,12 +28,10 @@ class New(Resource):
         books.append(book)
         return make_response(jsonify(books), HTTPStatus.CREATED)
 
-
-class Update(Resource):
     def put(self,book_id):
         book = next((book for book in books if book['id'] == book_id),None) 
         if not book:
-            return jsonify(message),HTTPStatus.NOT_FOUND
+            return make_response(jsonify(message),HTTPStatus.NOT_FOUND)
         data = request.put("https://flask-joy-api.herokuapp.com/")
         book.update(
         {
@@ -46,6 +42,41 @@ class Update(Resource):
             }
         )
         return make_response(jsonify(book))
+
+
+# class New(Resource):
+#     # def post(self):
+#     #     data = request.post("https://flask-joy-api.herokuapp.com/")
+#     #     title = data.get('book_title')
+#     #     author = data.get('book_author')
+#     #     publisher = data.get('publisher')
+#     #     description = data.get('description')
+#     #     book = {
+#     #         "id": len(books)+1,  
+#     #         "title": title,
+#     #         "author": author,
+#     #         "publisher": publisher,
+#     #         "description": description
+#     #         }
+#     #     books.append(book)
+#     #     return make_response(jsonify(books), HTTPStatus.CREATED)
+
+
+# class Update(Resource):
+#     def put(self,book_id):
+#         book = next((book for book in books if book['id'] == book_id),None) 
+#         if not book:
+#             return make_response(jsonify(message),HTTPStatus.NOT_FOUND)
+#         data = request.put("https://flask-joy-api.herokuapp.com/")
+#         book.update(
+#         {
+#             'title': data.get('book_title'),
+#             'author': data.get('book_author'),
+#             'publisher': data.get('publisher'),
+#             'description': data.get('description'),
+#             }
+#         )
+#         return make_response(jsonify(book))
 
 
 class Book(Resource):
