e7b303727999fce9407336f7ae07630c50e87435
Joy <joymakasi@hotmail.co.uk>
2022-02-27 21:02:25 +0000

    adding new code

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ class Books(Resource):
     def get(self):
         return make_response(jsonify(books))
 
+
+class New(Resource):
     def post(self):
         data = request.post("https://flask-joy-api.herokuapp.com/")
         title = data.get('book_title')
@@ -28,6 +30,8 @@ class Books(Resource):
         books.append(book)
         return make_response(jsonify(books), HTTPStatus.CREATED)
 
+
+class Update(Resource):
     def put(self,book_id):
         book = next((book for book in books if book['id'] == book_id),None) 
         if not book:
@@ -44,41 +48,6 @@ class Books(Resource):
         return make_response(jsonify(book))
 
 
-# class New(Resource):
-#     # def post(self):
-#     #     data = request.post("https://flask-joy-api.herokuapp.com/")
-#     #     title = data.get('book_title')
-#     #     author = data.get('book_author')
-#     #     publisher = data.get('publisher')
-#     #     description = data.get('description')
-#     #     book = {
-#     #         "id": len(books)+1,  
-#     #         "title": title,
-#     #         "author": author,
-#     #         "publisher": publisher,
-#     #         "description": description
-#     #         }
-#     #     books.append(book)
-#     #     return make_response(jsonify(books), HTTPStatus.CREATED)
-
-
-# class Update(Resource):
-#     def put(self,book_id):
-#         book = next((book for book in books if book['id'] == book_id),None) 
-#         if not book:
-#             return make_response(jsonify(message),HTTPStatus.NOT_FOUND)
-#         data = request.put("https://flask-joy-api.herokuapp.com/")
-#         book.update(
-#         {
-#             'title': data.get('book_title'),
-#             'author': data.get('book_author'),
-#             'publisher': data.get('publisher'),
-#             'description': data.get('description'),
-#             }
-#         )
-#         return make_response(jsonify(book))
-
-
 class Book(Resource):
     def get(self, book_id):
         book = next(
@@ -91,8 +60,8 @@ class Book(Resource):
 
 api.add_resource(Books, '/')
 api.add_resource(Book, '//<int:book_id>')
-# api.add_resource(Update, '//<int:book_id>')
-# api.add_resource(New, '//<int:book_id>')
+api.add_resource(Update, '//<int:book_id>')
+api.add_resource(New, '//<int:book_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
